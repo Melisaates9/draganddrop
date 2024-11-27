0c9bf634827b97650541f622352f6527f558e2e9
melisa serap ates <melisaserap@gmail.com>
2024-11-27 18:49:22 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import './App.css'
-
+import TextField from '@mui/material/TextField';
+import SendIcon from '@mui/icons-material/Send';
+import Button from '@mui/material/Button';
 function App() {
 
+const onDragEnd=()=>{
 
+}
   return (
+
     <>
-     
+<TextField id="standard-basic" label="Library" variant="standard" />
+<Button variant="contained" endIcon={<SendIcon />}>
+  Send
+</Button>
+
+
+
+
+
+     <DragDropContext onDragEnd={onDragEnd}>
+<Droppable droppableId="List">
+{provided => (
+          <div ref={provided.innerRef} {...provided.droppableProps}>
+          </div>
+        )}
+</Droppable>
+
+     </DragDropContext>
     </>
   )
 }
